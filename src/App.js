37a30091e6706e361465adcbf33ec3b497bd33dc
Leah Russell <leahsigridrussell@gmail.com>
2021-07-05 15:55:57 -0700

    Adding contact component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Portfolio from './components/Portfolio'
 
 function App() {
   const [titles] = useState([
-    { name: 'About', description: '' },
-    { name: 'Portfolio', description: '' },
-    { name: 'Contact', description: ''},
-    { name: 'Resume', description: '' },
+    { name: 'About' },
+    { name: 'Portfolio' },
+    { name: 'Contact' },
+    { name: 'Resume' },
   ]);
 
   const [currentTitle, setCurrentTitle] = useState(titles[0]);
@@ -47,15 +47,18 @@ export default App;
 // GIVEN a single-page application portfolio for a web developer
 // WHEN I load the portfolio
 // THEN I am presented with a page containing a header, a section for content, and a footer
-// WHEN I click on a navigation title
-// THEN I am presented with the corresponding section below the navigation without the page reloading and that title is highlighted
+
 // WHEN I am presented with the Contact section
 // THEN I see a contact form with fields for a name, an email address, and a message
+
 // WHEN I move my cursor out of one of the form fields without entering text
 // THEN I receive a notification that this field is required
+
 // WHEN I enter text into the email address field
 // THEN I receive a notification if I have entered an invalid email address
+
 // WHEN I am presented with the Resume section
 // THEN I see a link to a downloadable resume and a list of the developer’s proficiencies
+
 // WHEN I view the footer
 // THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn profiles, and their profile on a third platform (Stack Overflow, Twitter) 
